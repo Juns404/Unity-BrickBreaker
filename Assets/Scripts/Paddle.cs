ef04de2b3dc1856b2161004430524f80e1efe4db
Auza404 <53988417+Auza404@users.noreply.github.com>
2020-10-08 00:15:10 +0800

    Added paddle movement

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public bool isPlayer1;
+    public float speed;
+    Rigidbody2D rb;
+    float movement;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementPaddle();
+    }
+    void MovementPaddle()
+    {
+        if (isPlayer1)
+        {
+            movement = Input.GetAxisRaw("Horizontal");
+            rb.velocity = new Vector2(movement * speed, rb.velocity.y);
+        }
+    }
+}
