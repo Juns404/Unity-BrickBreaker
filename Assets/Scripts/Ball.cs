d2f6e2d725abdc9d2a3439c1d22a09a59b7df518
Auza404 <53988417+Auza404@users.noreply.github.com>
2020-10-08 10:35:48 +0800

    added ball movement with paddle, before ball launch.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody2D rb;
+    private bool launched;
+    private float speed = 5, movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        launched = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementBall();
+    }
+    private void MovementBall()
+    {
+        if (!launched)
+        {
+            movement = Input.GetAxisRaw("Horizontal");
+            rb.velocity = new Vector2(movement * speed, rb.velocity.y);
+        }
+    }
+}
